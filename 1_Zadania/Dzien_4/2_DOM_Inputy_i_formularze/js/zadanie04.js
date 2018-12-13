6fed3da211c6f2448ff39324b16927bcb69e94f4
megfan <megfa@yahoo.com>
2018-12-13 12:43:12 +0100

    repetition cd...

--- 1_Zadania/Dzien_4/2_DOM_Inputy_i_formularze/js/zadanie04.js
+++ 1_Zadania/Dzien_4/2_DOM_Inputy_i_formularze/js/zadanie04.js
@@ -1,3 +1,46 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('Dom done');
+
+
+    var creditCard = document.getElementById('name');
+    var alertCopy = document.querySelector('.form-group').firstElementChild;
+    var alert = alertCopy.cloneNode(true);
+    alertCopy.parentElement.appendChild(alert);
+    alert.innerHTML = '...';
+
+    var green = document.querySelectorAll('.form-control')[0];
+    console.log(green);
+
+    creditCard.addEventListener('change', function(e){
+        e.target.value = creditCard.value;
+        Validation(e);
+    })
+    function Validation(e){
+        if(e.target.value[0] == 4){
+            if(creditCard.value.length < 13){
+                alert.innerHTML = 'Karta Visa ma od 13 do 16 cyfr';
+            }else{
+                alert.innerHTML = 'Karta Visa poprawna';
+            }           
+        }else if(creditCard.value[0] == 5){
+            if(creditCard.value.length != 16){
+                alert.innerHTML = 'Karta MasterCard ma 16 cyfr';
+            }else{
+                alert.innerHTML = 'Karta MasterCard poprawna';
+            }           
+        }else if(creditCard.value[0] == 3 && creditCard.value[1] == 4 || creditCard.value[1] == 7){
+            if(creditCard.value.length != 15){
+                alert.innerHTML = 'Karta American Express ma 15 cyfr';
+                alert.parentElement.children[1].style.border = 'thin solid green';
+            }else{
+                alert.innerHTML = 'Karta American Express poprawna';
+            } 
+        }else{
+            console.log('karta Unknown');
+        }
+    }
+
+
+
+});
