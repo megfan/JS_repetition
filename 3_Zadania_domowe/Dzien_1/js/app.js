9f9653dbb5789ec9dbf504a9b823a597cc0f9e0c
megfan <megfa@yahoo.com>
2018-12-13 22:03:07 +0100

    repetition cd...

--- 3_Zadania_domowe/Dzien_1/js/app.js
+++ 3_Zadania_domowe/Dzien_1/js/app.js
@@ -1,42 +1,42 @@
 
-function getNumber(n,array) {
+function getNumber(a, arr){
+    var check = arr.indexOf(a);
+    if(check === -1){
+        console.log(false);
+        return false;
+    }else{
+        console.log(true + a);
+        return true;
+    }
+};
+getNumber(5, [33,54,2,5,1,4,100]);
 
-    var p = n;
+// function getNumber(n,array) {
+//     var p = n;
+//     for (var i = 0; i < array.length; i++) {
+//             var x = array[i];
+//             // console.log(x);
+//             if(p === x) {
+//                 console.log(true);
+//             }
+//     }
+// }
+// getNumber(2, [33,54,2,1,4,100]);
+// getNumber(5, [33,54,2,1,4,100]);
 
-    for (var i = 0; i < array.length; i++) {
-            var x = array[i];
-            // console.log(x);
-
-            if(p === x) {
-                console.log(true);
+// //zad 2
+function createMatrix(rows,columns){
+    var arr =[];
+    for(var i =0; i<rows; i++){
+        arr[i]= [];
+        for(var j=0; j<columns; j++){
+            if(i === j){
+            arr[i][j] = 1;
+            }else{
+                arr[i][j]=0;
             }
-
-    }
-}
-
-getNumber(2, [33,54,2,1,4,100]);
-getNumber(5, [33,54,2,1,4,100]);
-
-
-//zad 2
-
-function createIdentityMatrix(rows, columns) {
-
-    var array = [];
-
-    for (var i = 0; i < rows; i++) {
-        array[i] = [];
-        for (var j = 0; j < columns; j++) {
-           if(i === j){
-               array[i][j] = 1;
-           }else {
-               array[i][j] = 0;
-           }
-
         }
-    }
-    return array;
-
+    }console.log(arr);
+    return arr;
 }
-
-createIdentityMatrix(4,4);
+createMatrix(4,4);
--- 3_Zadania_domowe/Dzien_1/js/planets.js
+++ 3_Zadania_domowe/Dzien_1/js/planets.js
@@ -32,3 +32,33 @@ var planets = [
         numberOfMoons: 13
     }
 ];
+
+function sortPlanets(property){
+    var sortOrder = 1;
+    if(property[0] === '-'){
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function(a,b){
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 
+        : 0;
+        return result * sortOrder;
+    }
+
+    // planets.sort(function(a,b){
+    //     return a-b;
+    // })
+    // var arr = [];
+    // for(var i=0; i<planets.length; i++){
+    //     var number = planets[i].numberOfMoons;
+    //     console.log(number);
+    //     for(var j=0; j<arr.length; j++){
+    //         if(number > arr[j].numberOfMoons){
+    //             arr.push(planets[i]);
+    //         }
+    //     }
+    // }console.log(arr);
+    // return arr;
+}
+console.log(planets.sort(sortPlanets('numberOfMoons')));
+planets.reverse(sortPlanets('numberOfMoons'));
