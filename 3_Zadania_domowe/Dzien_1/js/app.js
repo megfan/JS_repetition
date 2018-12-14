cf25c8f7b4b4c0922ddf52eaa3f12446e6a80ba8
megfan <megfa@yahoo.com>
2018-12-14 12:08:36 +0100

    powtórka z rozrywki

--- 3_Zadania_domowe/Dzien_1/js/app.js
+++ 3_Zadania_domowe/Dzien_1/js/app.js
@@ -1,15 +1,15 @@
 
-function getNumber(a, arr){
-    var check = arr.indexOf(a);
-    if(check === -1){
-        console.log(false);
-        return false;
-    }else{
-        console.log(true + a);
-        return true;
-    }
-};
-getNumber(5, [33,54,2,5,1,4,100]);
+// function getNumber(a, arr){
+//     var check = arr.indexOf(a);
+//     if(check === -1){
+//         console.log(false);
+//         return false;
+//     }else{
+//         console.log(true + a);
+//         return true;
+//     }
+// };
+// getNumber(5, [33,54,2,5,1,4,100]);
 
 // function getNumber(n,array) {
 //     var p = n;
@@ -25,18 +25,78 @@ getNumber(5, [33,54,2,5,1,4,100]);
 // getNumber(5, [33,54,2,1,4,100]);
 
 // //zad 2
-function createMatrix(rows,columns){
-    var arr =[];
-    for(var i =0; i<rows; i++){
-        arr[i]= [];
-        for(var j=0; j<columns; j++){
-            if(i === j){
-            arr[i][j] = 1;
-            }else{
-                arr[i][j]=0;
-            }
-        }
-    }console.log(arr);
-    return arr;
+// function createMatrix(rows,columns){
+//     var arr =[];
+//     for(var i =0; i<rows; i++){
+//         arr[i]= [];
+//         for(var j=0; j<columns; j++){
+//             if(i === j){
+//             arr[i][j] = 1;
+//             }else{
+//                 arr[i][j]=0;
+//             }
+//         }
+//     }console.log(arr);
+//     return arr;
+// }
+// createMatrix(4,4);
+
+
+// zadania dodatkowe
+//1
+// function addTehSameNUmbers(n, arr){
+//     var sum = 0;
+//     arr.forEach(element => {
+//         if(element != n){
+//             return sum = null;
+//         }else{
+//             sum += element ;
+//             console.log(sum);
+//         }
+//     });return sum;
+// }
+
+// console.log(addTehSameNUmbers(7,[4,10]));
+
+//2
+
+// function factors(n){
+//     var resultArr =[];
+//     for(var i=n; i>=0; i--){
+//         if(n%i == 0){
+//             console.log(i);
+//             resultArr.push(i);
+//         }
+//     }return resultArr;
+// }
+
+// console.log(factors(54));
+
+//3
+// function grtMissingElem(arr){
+//     var result = null;
+//     for(var i=0; i<arr.length; i++){
+//         var x = arr[i];
+//         if(x-i!=1){
+//             result = arr[i]-1;
+//         }
+//     }return result
+// };
+// console.log(grtMissingElem([1,2,3,4,5,6,7,8,9]));
+
+//4
+function getLastNumber(n, arr){
+    var newArr=[];
+    console.log(typeof(n));
+    if(typeof(n) != 'number'){
+        return newArr;
+    }else{
+        var x = arr.length;
+        console.log(x);
+        for(var i=x-1; i>=x-n; i--){
+            newArr.push(arr[i]);
+        }console.log(newArr);
+        return newArr;
+    }
 }
-createMatrix(4,4);
+getLastNumber(2,[1,2,3,4,5,6,7,8,9]);
--- 3_Zadania_domowe/Dzien_1/js/planets.js
+++ 3_Zadania_domowe/Dzien_1/js/planets.js
@@ -33,32 +33,23 @@ var planets = [
     }
 ];
 
-function sortPlanets(property){
-    var sortOrder = 1;
-    if(property[0] === '-'){
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function(a,b){
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 
-        : 0;
-        return result * sortOrder;
-    }
 
-    // planets.sort(function(a,b){
-    //     return a-b;
-    // })
-    // var arr = [];
-    // for(var i=0; i<planets.length; i++){
-    //     var number = planets[i].numberOfMoons;
-    //     console.log(number);
-    //     for(var j=0; j<arr.length; j++){
-    //         if(number > arr[j].numberOfMoons){
-    //             arr.push(planets[i]);
-    //         }
-    //     }
-    // }console.log(arr);
-    // return arr;
-}
-console.log(planets.sort(sortPlanets('numberOfMoons')));
-planets.reverse(sortPlanets('numberOfMoons'));
+var sortPlanets = planets.sort(function(a,b){
+    return b.numberOfMoons - a.numberOfMoons;
+})
+console.log(sortPlanets);
+
+// function sortPlanets(property){
+//     var sortOrder = 1;
+//     if(property[0] === '-'){
+//         sortOrder = -1;
+//         property = property.substr(1);
+//     }
+//     return function(a,b){
+//         var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 
+//         : 0;
+//         return result * sortOrder;
+//     }
+
+// console.log(planets.sort(sortPlanets('numberOfMoons')));
+// planets.reverse(sortPlanets('numberOfMoons'));
