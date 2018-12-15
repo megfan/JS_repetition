54647cd9929f5777247e70c3b08fef6b513acbc9
megfan <megfa@yahoo.com>
2018-12-15 11:38:48 +0100

    powtórka z rozrywki

--- 3_Zadania_domowe/Dzien_4/js/pizza.js
+++ 3_Zadania_domowe/Dzien_4/js/pizza.js
@@ -3,7 +3,39 @@
     var products = document.querySelectorAll('.checkbox input');
     var allAdditions = products[0];
     var allClear = products[products.length-1];
-    console.log(allClear);
 
-    var sum =0;
-
+    var arrOfCheckBox = [];
+    for(var i =1; i< products.length-1; i++){
+        arrOfCheckBox.push(products[i]);
+    }
+    var sum = 0;
+    arrOfCheckBox.forEach((elem) => {
+        elem.addEventListener('change', function(){
+            if(this.checked === true){
+                sum += Number(this.dataset.price);
+                price.innerHTML = sum.toFixed(2) + 'zł';
+                allClear.checked = false;
+                allAdditions.checked = false;
+            }else{
+                sum -= Number(this.dataset.price);
+                price.innerHTML = sum.toFixed(2) + 'zł';
+                allClear.checked = false;
+                allAdditions.checked = false;
+            }
+        })
+    });
+    allAdditions.addEventListener('change', function(){
+        arrOfCheckBox.forEach((elem) => {
+            elem.checked = true;
+            sum += Number(elem.dataset.price);
+            price.innerHTML = sum.toFixed(2) + 'zł';
+        })
+    });
+    allClear.addEventListener('change', function(){
+        arrOfCheckBox.forEach((elem) => {
+        elem.checked = false;
+        })
+        allAdditions.checked = false;
+        sum = 0;
+        price.innerHTML = sum + 'zł';
+    })
